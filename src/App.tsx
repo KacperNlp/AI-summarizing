9c1651bfb9c0bd6a28e8058ce8dd537a613cba09
KacperNlp <kacper.nlp@wp.pl>
2025-02-15 17:22:54 +0100

    feat: install xenova/transformers package - #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
 import { useState } from "react";
+import { pipeline } from "@xenova/transformers";
 
 import { AppButton } from "./components/AppButton";
 import { AppLoader } from "./components/AppLoader";
 
 function App() {
   const [isAnalyzing, setIsAnalyzing] = useState(false);
+  const [message, setMessage] = useState(false);
 
-  const analyzeWebsite = () => {
+  async function summarizeLocally(text) {
+    const summarizer = await pipeline("summarization");
+    const summary = await summarizer(text, { max_length: 100 });
+    return summary[0]?.summary_text;
+  }
+
+  async function analyzeWebsite() {
     setIsAnalyzing(true);
 
-    setTimeout(() => {
-      setIsAnalyzing(false);
-    }, 5000);
+    await summarizeLocally(message);
+    setIsAnalyzing(false);
+  }
+
+  const updateInputValue = (e: Event) => {
+    if (!e || e === null) return;
+
+    setMessage(e.target.value);
   };
 
   return (
@@ -21,6 +34,7 @@ function App() {
           <span className="font-bold text-purple-600">AI</span> Summaring
         </h1>
         <AppButton onClick={analyzeWebsite}>Przeanalizuj stronę!</AppButton>
+        <input type="text" onChange={updateInputValue} />
       </section>
       {isAnalyzing && <AppLoader />}
     </>
